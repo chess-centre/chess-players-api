dd8b735cad0489aed43fffb7037796016694a33e
Matt D. Webb <36933715+matt-d-webb@users.noreply.github.com>
2020-09-13 14:25:05 +0100

    refactor: updated promises to async await (closes #1) (#2)
    * refactor: updated promises to async await
    
    * tests: update promise handling and include happy path tests
    
    * fix: linting issues from codacy scan - closes #1

---
+++ __tests__/handler.test.ts
@@ -0,0 +1,37 @@
+const handler = require("../handler");
+const mockData = require("./mock.data.json");
+
+describe("HTTP calls", () => {
+    test("GET /players -> getPlayers()", async () => {
+        expect(handler.getPlayers()).resolves.toBe(mockData);
+    });
+
+    test("GET /player/{id} -> getPlayer()", async () => {
+        expect(handler.getPlayer({ pathParameters: {
+            id: 1
+        }})).resolves.toBe(mockData);
+
+        // TODO: add "Player not found" case
+    });
+
+    test("POST /player -> createPlayer()", async () => {
+        expect(handler.createPlayer({ body: JSON.stringify({
+            name: "player name",
+            country: "player country",
+            rating: 1000 })
+        })).resolves.toBe(mockData);
+
+        // TODO: exception for "Player must have an id, name, rating"
+    });
+
+    test("PUT /player/{id} -> updatePlayer()", async () => {
+        expect(handler.updatePlayer({
+            pathParameters: {
+                id: 1
+            },
+            body: JSON.stringify({
+                rating: 2000
+            })
+        })).resolves.toBe({ ...mockData, rating: 2000 });
+    });
+});
--- handler.ts
+++ handler.ts
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
-import * as AWS from 'aws-sdk';
-import { APIGatewayProxyHandler, APIGatewayProxyEvent, Context } from 'aws-lambda';
+import * as AWS from "aws-sdk";
+import { APIGatewayProxyHandler, APIGatewayProxyEvent } from "aws-lambda";
 
-const db = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
+const db = new AWS.DynamoDB.DocumentClient({ apiVersion: "2012-08-10" });
 const playersTable = process.env.PLAYERS_TABLE || "players";
 
 interface Player {
@@ -39,14 +39,14 @@ const sortByDate: any = (a: Player, b: Player) => (a.createdAt > b.createdAt) ? 
 /**
  * POST /players
  */
-module.exports.createPlayer = (event: APIGatewayProxyEvent, context: Context, callback: Function): Promise<APIGatewayProxyHandler> => {
+module.exports.createPlayer = async (event: APIGatewayProxyEvent, callback: Function): Promise<APIGatewayProxyHandler> => {
   const { id, name, rating, country }: Player = JSON.parse(event.body || '{}');
 
   if (!id || !name || !rating) {
     return callback(
       null,
       response(StatusCode.BadRequest, {
-        error: 'Player must have a id, name and rating'
+        error: "Player must have an id, name and rating"
       })
     );
   }
@@ -59,63 +59,53 @@ module.exports.createPlayer = (event: APIGatewayProxyEvent, context: Context, ca
     createdAt: new Date().toISOString()
   };
 
-  return db
-    .put({
-      TableName: playersTable,
-      Item: player
-    })
-    .promise()
-    .then(() => {
-      callback(null, response(StatusCode.Created, player));
-    })
-    .catch((err) => response(null, response(err.statusCode, err)));
+  const res = await db.put({ TableName: playersTable, Item: player }).promise()
+                      .catch((err) => response(null, response(err.statusCode, err)));
+  
+  return callback(null, response(StatusCode.Created, res.Item));
+
 };
 
 /**
  * GET /players
  */
-module.exports.getAllPlayers = (event: APIGatewayProxyEvent, context: Context, callback: Function): Promise<APIGatewayProxyHandler> => {
-  return db
-    .scan({
-      TableName: playersTable
-    })
-    .promise()
-    .then((res) => {
-      callback(null, response(StatusCode.Success, res.Items?.sort(sortByDate)));
-    })
-    .catch((err) => callback(null, response(err.statusCode, err)));
+module.exports.getPlayers = async (event: APIGatewayProxyEvent, callback: Function): Promise<APIGatewayProxyHandler> => {
+  const res = await db.scan({ TableName: playersTable }).promise()
+    .catch((err) => callback(null, response(err.statusCode, err)))
+
+  return callback(null, response(StatusCode.Success, res.Items?.sort(sortByDate)));
 };
 
 /**
  * GET /player/{id} 
  */
-module.exports.getPlayer = (event: APIGatewayProxyEvent, context: Context, callback: Function): Promise<APIGatewayProxyHandler>  => {
+module.exports.getPlayer = async (event: APIGatewayProxyEvent, callback: Function): Promise<APIGatewayProxyHandler> => {
   const { id } = event.pathParameters!;
 
   const params = {
-      TableName: playersTable,
-      Key: {
-        id
-      }
+    TableName: playersTable,
+    Key: {
+      id
+    }
   };
 
-  return db
-    .get(params)
-    .promise()
-    .then((res) => {
-      if (res.Item) callback(null, response(StatusCode.Success, res.Item));
-      else callback(null, response(StatusCode.NotFound, { error: 'Player not found' }));
-    })
+  const res = await db.get(params).promise()
     .catch((err) => callback(null, response(err.statusCode, err)));
+
+  if (res.Item) {
+    return callback(null, response(StatusCode.Success, res.Item))
+  } else {
+    return callback(null, response(StatusCode.NotFound, { error: "Player not found" }))
+  }
 };
 
 
 /**
  * PUT /player/{id} 
  */
-module.exports.updatePost = (event: APIGatewayProxyEvent, context: Context, callback: Function): Promise<APIGatewayProxyHandler>  => {
+module.exports.updatePlayer = async (event: APIGatewayProxyEvent, callback: Function): Promise<APIGatewayProxyHandler> => {
   const { id } = event.pathParameters!;
-  const reqBody = JSON.parse(event.body || '{}');
+  const reqBody = JSON.parse(event.body || "{}");
   const { rating }: Player = reqBody;
 
   const params = {
@@ -123,19 +113,17 @@ module.exports.updatePost = (event: APIGatewayProxyEvent, context: Context, call
       id: id
     },
     TableName: playersTable,
-    ConditionExpression: 'attribute_exists(id)',
-    UpdateExpression: 'SET rating = :rating',
+    ConditionExpression: "attribute_exists(id)",
+    UpdateExpression: "SET rating = :rating",
     ExpressionAttributeValues: {
-      ':rating': rating,
+      ":rating": rating,
     },
-    ReturnValues: 'ALL_NEW'
+    ReturnValues: "ALL_NEW"
   };
 
-  return db
-    .update(params)
-    .promise()
-    .then((res) => {
-      callback(null, response(StatusCode.Success, res.Attributes));
-    })
-    .catch((err) => callback(null, response(err.statusCode, err)));
+  const res = await db.update(params).promise()
+                        .catch((err) => callback(null, response(err.statusCode, err)));
+
+  return callback(null, response(StatusCode.Success, res.Attributes));
+    
 };
